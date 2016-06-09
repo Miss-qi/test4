3feb77ef895317738edc806119c36e83e3d5a43e
Miss-qi <1370322806@qq.com>
2016-06-09 22:54:51 +0800

    first commit

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -1,7 +1,11 @@
 'use strict';
 
 function double_to_one(collection) {
-
+	var arr = [];
+	for(var i = 0; i < collection.length; i++){
+		arr=arr.concat(collection[i]);
+	}
+	return arr;	
   //在这里写入代码
 }
 
--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -1,7 +1,13 @@
 'use strict';
 
 function double_to_one(collection) {
-
+	var temp = [];
+	for(var i in collection){
+		if(temp.indexOf(collection[i]) == -1){
+			temp.push(collection[i]);
+		}
+	}
+	return temp;
   //在这里写入代码
 }
 
